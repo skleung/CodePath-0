6d6efb86e86da20f6228c8337bb783bfaa262910
Sherman Leung <shermankleung@gmail.com>
2015-03-10 04:01:22 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# tips
+An iOS application to calculate tips based on the [video walkthrough](http://courses.codepath.com/snippets/intro_to_ios/thanks_for_applying_stanford#heading-prework) presented by CodePath
+
+Features include tip percentage selection, and a segmented button to determine the number of people for the bill.
+
+I put in about 2 hours of work including the video walkthrough.
+
+*Note to CodePath
+Unfortunately I can't include the licecap GIF, for some reason I can't get it to work after trying [some suggestions](http://forum.cockos.com/showthread.php?t=72079). Hopefully this won't mark me elligible for the course application.
--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // define outlets
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var numPeople: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        totalLabel.text = "$0.00";
     }
 
+    // links the tap gesture to ending the keyboard
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var tipPercentages = [0.18, 0.2, 0.22]
+        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
+        var billFieldText = billField.text
+        if (billFieldText.hasPrefix("$")){
+            billFieldText = billFieldText.substringFromIndex(advance(billFieldText.startIndex,1))
+        }
+        var numPersons = Double(numPeople.selectedSegmentIndex+1)
+        var billAmount = (billFieldText as NSString).doubleValue
+        var tip = billAmount * tipPercentage
+        var total = (billAmount + tip)/numPersons
+        if (!billField.text.hasPrefix("$")){
+            billField.text = "$"+billField.text
+        }
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
